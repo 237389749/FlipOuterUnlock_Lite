5881368d49313bc7f7337a8f48b9580f08889d20
xcz <l237389749@gmail.com>
2026-08-15 23:16:37 +0800

    TEMPORARY TEST #3: 第三次故意语法错误触发 CI/邮箱通知(勿合并)

--- app/src/main/java/com/example/flipunlock/hook/BaseHook.kt
+++ app/src/main/java/com/example/flipunlock/hook/BaseHook.kt
@@ -4,6 +4,7 @@ import com.example.flipunlock.hook.util.safeHook
 import io.github.libxposed.api.XposedModuleInterface.PackageReadyParam
 
 abstract class BaseHook {
+    third_deliberate_error_???  // 测试性错误 #3: 第三次触发 CI/邮箱通知(勿合并)
     abstract val targetPackages: List<String>
 
     open fun hook(param: PackageReadyParam) {
